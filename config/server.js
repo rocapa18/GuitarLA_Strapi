4db1c5e46276816303208416931606166f0ec9e6
rocapa18 <roberto.pavano@gmail.com>
2022-05-20 17:52:35 -0300

    Deploiment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7b2937021e709d0187a5f1478ccf5bf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1fe1606e-f5a8-4eec-a2d5-d6f33a4a0e92'
+};
